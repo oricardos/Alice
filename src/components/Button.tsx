634fcc8d6508aee3b8691df61b9363e2451e1c0b
oricardos <ricardo.rs95@gmail.com>
2022-10-17 23:26:59 -0300

    feat(geral): add components

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,19 +1,20 @@
-import { ReactNode } from 'react';
+import { ButtonHTMLAttributes, ReactNode } from 'react';
 import { clsx } from 'clsx';
 import { Slot } from '@radix-ui/react-slot'
 
-export interface ButtonProps {
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     color?: 'bg-primary' | 'bg-secondary' | 'bg-success' | 'bg-warning' | 'bg-danger' ;
     children: ReactNode;
     asChild?: boolean;
 }
 
-export function Button({ color = 'bg-primary', children, asChild }: ButtonProps){
+export function Button({ color = 'bg-primary', children, asChild, className, ...props }: ButtonProps){
     const Comp = asChild ? Slot : 'button';
     return (
         <Comp 
             className={clsx(
-                'py-4 px-3 rounded font-semibold text-black text-sm w-full transition-colors', 
+                'py-3 px-4 rounded font-semibold text-black text-sm w-full transition-colors',
+                className,
                 {
                     'bg-cyan-500': color === 'bg-primary',
                     'bg-gray-200': color === 'bg-secondary',
@@ -22,6 +23,7 @@ export function Button({ color = 'bg-primary', children, asChild }: ButtonProps)
                     'bg-red-600': color === 'bg-danger',
                 }
             )}
+            {...props}
         >
             {children}
         </Comp>
